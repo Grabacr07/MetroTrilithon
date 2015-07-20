f945da5c4a125a627b5906e1acc7716fe8e6e3aa
Grabacr07 <grabacr07+github@gmail.com>
2015-07-21 01:23:10 +0900

    Add constructor of SerializableProperty<T>

--- source/MetroTrilithon.Desktop/Serialization/SerializableProperty.cs
+++ source/MetroTrilithon.Desktop/Serialization/SerializableProperty.cs
@@ -10,5 +10,9 @@ namespace MetroTrilithon.Serialization
 		public SerializableProperty(string key) : this(key, default(T)) { }
 
 		public SerializableProperty(string key, T defaultValue) : base(key, ApplicationSettingsProvider.Default, defaultValue) { }
+
+		public SerializableProperty(string key, ISerializationProvider provider) : base(key, provider) { }
+
+		public SerializableProperty(string key, ISerializationProvider provider, T defaultValue) : base(key, provider, defaultValue) { }
 	}
 }
