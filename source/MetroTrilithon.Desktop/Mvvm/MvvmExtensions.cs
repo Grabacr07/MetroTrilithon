546cf5c8ce5a7db49bf02f463ddcfcad347cf823
Grabacr07 <grabacr07+github@gmail.com>
2015-07-21 17:56:14 +0900

    Add WindowViewModel and extensions for mvvm.

---
+++ source/MetroTrilithon.Desktop/Mvvm/MvvmExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Livet;
+
+namespace MetroTrilithon.Mvvm
+{
+	public static class MvvmExtensions
+	{
+		/// <summary>
+		/// <see cref="IDisposable"/> オブジェクトを、指定した <see cref="ViewModel"/> の <see cref="ViewModel.CompositeDisposable"/> に追加します。
+		/// </summary>
+		public static void AddTo(this IDisposable disposable, ViewModel vm)
+		{
+			vm.CompositeDisposable.Add(disposable);
+		}
+	}
+}
