9e8be60f6d72c58807728312523d10a96221ea31
Grabacr07 <grabacr07+github@gmail.com>
2015-07-23 16:45:41 +0900

    Minor changeds in AddTo(IDisposable)

--- source/MetroTrilithon.Desktop/Mvvm/MvvmExtensions.cs
+++ source/MetroTrilithon.Desktop/Mvvm/MvvmExtensions.cs
@@ -13,17 +13,31 @@ namespace MetroTrilithon.Mvvm
 		/// <summary>
 		/// <see cref="IDisposable"/> オブジェクトを、指定した <see cref="ViewModel"/> の <see cref="ViewModel.CompositeDisposable"/> に追加します。
 		/// </summary>
-		public static void AddTo(this IDisposable disposable, ViewModel vm)
+		public static T AddTo<T>(this T disposable, ViewModel vm) where T : IDisposable
 		{
+			if (vm == null)
+			{
+				disposable.Dispose();
+				return disposable;
+			}
+
 			vm.CompositeDisposable.Add(disposable);
+			return disposable;
 		}
 
 		/// <summary>
 		/// <see cref="IDisposable"/> オブジェクトを、指定した <see cref="ICompositeDisposable.CompositeDisposable"/> に追加します。
 		/// </summary>
-		public static void AddTo(this IDisposable disposable, ICompositeDisposable compositDisposable)
+		public static T AddTo<T>(this T disposable, ICompositeDisposable obj) where T : IDisposable
 		{
-			compositDisposable.CompositeDisposable.Add(disposable);
+			if (obj == null)
+			{
+				disposable.Dispose();
+				return disposable;
+			}
+
+			obj.CompositeDisposable.Add(disposable);
+			return disposable;
 		}
 
 
