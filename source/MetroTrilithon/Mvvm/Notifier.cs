0f0a3cc285de7d8d7d0e1a5b4e7312f771e614fc
Grabacr07 <grabacr07+github@gmail.com>
2015-07-29 02:09:45 +0900

    Change Notifier.PropertyChanged event into explicit implementation.

--- source/MetroTrilithon/Mvvm/Notifier.cs
+++ source/MetroTrilithon/Mvvm/Notifier.cs
@@ -7,13 +7,26 @@ using System.Threading.Tasks;
 
 namespace MetroTrilithon.Mvvm
 {
+	/// <summary>
+	/// プロパティ変更通知をサポートします。
+	/// </summary>
 	public class Notifier : INotifyPropertyChanged
 	{
-		public event PropertyChangedEventHandler PropertyChanged;
+		private event PropertyChangedEventHandler _propertyChanged;
 
+		event PropertyChangedEventHandler INotifyPropertyChanged.PropertyChanged
+		{
+			add { this._propertyChanged += value; }
+			remove { this._propertyChanged -= value; }
+		}
+
+		/// <summary>
+		/// <see cref="INotifyPropertyChanged.PropertyChanged"/> イベントを発生させます。
+		/// </summary>
+		/// <param name="propertyName"></param>
 		protected virtual void RaisePropertyChanged([CallerMemberName] string propertyName = null)
 		{
-			this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+			this._propertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 		}
 	}
 }
