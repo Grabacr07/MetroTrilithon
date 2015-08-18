e0fb31ccc46ac71dc430f683efc84408e536bd6e
Grabacr07 <grabacr07+github@gmail.com>
2015-08-19 02:08:49 +0900

    Add Do<T> method (evil).

--- source/MetroTrilithon/Linq/EnumerableEx.cs
+++ source/MetroTrilithon/Linq/EnumerableEx.cs
@@ -12,6 +12,15 @@ namespace MetroTrilithon.Linq
 			yield return value;
 		}
 
+		public static IEnumerable<T> Do<T>(this IEnumerable<T> source, Action<T> action)
+		{
+			foreach (var item in source)
+			{
+				action(item);
+				yield return item;
+			}
+		}
+
 		public static string JoinString<T>(this IEnumerable<T> source, string separator)
 		{
 			return string.Join(separator, source is IEnumerable<string> ? (IEnumerable<string>)source : source.Select(x => x.ToString()));
