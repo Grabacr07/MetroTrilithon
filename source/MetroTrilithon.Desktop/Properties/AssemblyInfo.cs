5f2c3ecc222d532ef5eec3f7cd653beecdfcd64c
Grabacr07 <grabacr07+github@gmail.com>
2015-12-16 03:52:34 +0900

    Update version.

--- source/MetroTrilithon.Desktop/Properties/AssemblyInfo.cs
+++ source/MetroTrilithon.Desktop/Properties/AssemblyInfo.cs
@@ -3,9 +3,6 @@ using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Markup;
 
-// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
-// アセンブリに関連付けられている情報を変更するには、
-// これらの属性値を変更してください。
 [assembly: AssemblyTitle("MetroTrilithon.Desktop")]
 [assembly: AssemblyDescription("Utilities for Windows Desktop apps")]
 [assembly: AssemblyConfiguration("")]
@@ -15,20 +12,11 @@ using System.Windows.Markup;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// ComVisible を false に設定すると、その型はこのアセンブリ内で COM コンポーネントから
-// 参照不可能になります。COM からこのアセンブリ内の型にアクセスする場合は、
-// その型の ComVisible 属性を true に設定してください。
 [assembly: ComVisible(false)]
-
 [assembly: Guid("4e2eb2e0-e5fe-4feb-a3e5-5f2f05cd1a67")]
 
 [assembly: ThemeInfo(
-	// テーマ固有のリソース ディクショナリが置かれている場所
-	// (リソースがページ、またはアプリケーション リソース ディクショナリに見つからない場合に使用されます)
 	ResourceDictionaryLocation.None,
-
-	// 汎用リソース ディクショナリが置かれている場所
-	// (リソースがページ、アプリケーション、またはいずれのテーマ固有のリソース ディクショナリにも見つからない場合に使用されます)
 	ResourceDictionaryLocation.SourceAssembly
 )]
 
@@ -36,14 +24,5 @@ using System.Windows.Markup;
 [assembly: XmlnsDefinition("http://schemes.grabacr.net/winfx/2015/personal/converters", "MetroTrilithon.Converters")]
 [assembly: XmlnsDefinition("http://schemes.grabacr.net/winfx/2015/personal/interactivity", "MetroTrilithon.Interactivity")]
 
-// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
-// 既定値にすることができます:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.4.0")]
+[assembly: AssemblyVersion("0.1.5.0")]
+[assembly: AssemblyInformationalVersion("0.1.5")]
--- source/MetroTrilithon/Properties/AssemblyInfo.cs
+++ source/MetroTrilithon/Properties/AssemblyInfo.cs
@@ -1,11 +1,6 @@
 ﻿using System.Resources;
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("MetroTrilithon")]
 [assembly: AssemblyDescription("Utilities for any apps (.NET Framework, Windows Store, Windows Phone, Xamarin, ...)")]
 [assembly: AssemblyConfiguration("")]
@@ -15,14 +10,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.1.0")]
+[assembly: AssemblyVersion("0.1.2.0")]
+[assembly: AssemblyInformationalVersion("0.1.2")]
