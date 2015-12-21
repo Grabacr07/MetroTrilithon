5f7ba184df353bc2d983b1df9220ead1c40f6338
Grabacr07 <grabacr07+github@gmail.com>
2015-12-22 04:04:26 +0900

    Add ShellLink feature.

---
+++ source/MetroTrilithon.Desktop/Desktop/ShellLink.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
+using MetroTrilithon.Desktop.Interop;
+
+namespace MetroTrilithon.Desktop
+{
+	public static class ShellLink
+	{
+		public static void Create(string path)
+		{
+			var type = Type.GetTypeFromCLSID(CLSID.ShellLink);
+			var psl = (IShellLink)Activator.CreateInstance(type);
+
+			psl.SetPath(Assembly.GetExecutingAssembly().Location);
+
+			// ReSharper disable once SuspiciousTypeConversion.Global
+			var ppf = (IPersistFile)psl;
+
+			ppf.Save(path, false);
+		}
+	}
+}
