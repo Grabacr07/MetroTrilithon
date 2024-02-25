028b9b8469876ada8cd14804b803c0140bf38fbe
Grabacr07 <grabacr07+github@gmail.com>
2024-02-26 01:08:13 +0900

    Add math utils

---
+++ src/MetroTrilithon/Utils/DoubleUtil.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetroTrilithon.Utils;
+
+public static class DoubleUtil
+{
+    public static double EnsureRange(this double value, double max)
+        => Math.Min(value, max);
+
+    public static double EnsureRange(this double value, double min, double max)
+        => Math.Max(min, Math.Min(value, max));
+}
---
+++ src/MetroTrilithon/Utils/Int32Util.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetroTrilithon.Utils;
+
+public static class Int32Util
+{
+    public static int EnsureRange(this int value, int max)
+        => Math.Min(value, max);
+
+    public static int EnsureRange(this int value, int min, int max)
+        => Math.Max(min, Math.Min(value, max));
+}
