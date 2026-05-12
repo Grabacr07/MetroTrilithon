2e2cccb5a36a573f71d992b76fc35ac59a5942db
Grabacr07 <grabacr07+github@gmail.com>
2026-05-13 07:52:08 +0900

    feat: add CardAction corner radius support

--- src/Amethystra.Desktop/UI/Controls/CardActionExtensions.cs
+++ src/Amethystra.Desktop/UI/Controls/CardActionExtensions.cs
@@ -5,6 +5,8 @@ namespace Amethystra.UI.Controls;
 
 public static class CardActionExtensions
 {
+    #region ChevronGlyph attached property
+
     public static readonly DependencyProperty ChevronGlyphProperty
         = DependencyProperty.RegisterAttached(
             nameof(ChevronGlyphProperty).GetPropertyName(),
@@ -17,4 +19,23 @@ public static class CardActionExtensions
 
     public static string? GetChevronGlyph(CardAction element)
         => (string?)element.GetValue(ChevronGlyphProperty);
+
+    #endregion
+
+    #region CornerRadius attached property
+
+    public static readonly DependencyProperty CornerRadiusProperty
+        = DependencyProperty.RegisterAttached(
+            nameof(CornerRadiusProperty).GetPropertyName(),
+            typeof(CornerRadius),
+            typeof(CardActionExtensions),
+            new PropertyMetadata(new CornerRadius(4)));
+
+    public static void SetCornerRadius(CardAction element, CornerRadius value)
+        => element.SetValue(CornerRadiusProperty, value);
+
+    public static CornerRadius GetCornerRadius(CardAction element)
+        => (CornerRadius)element.GetValue(CornerRadiusProperty);
+
+    #endregion
 }
